ee27728f4e75ae1a05e00ce34ca626d0c02328e5
facurom <facundortt@gmail.com>
2023-05-15 09:55:00 -0300

    fdg

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,14 +1,47 @@
 const winston = require('winston')
 
+const customLevelsOptions = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        debug: 4
+    },
+    colors: {
+        fatal: 'red',
+        error: 'orange',
+        warning: 'yellow',
+        info: 'blue',
+        debug: 'white'
+    }
+}
+
+// const logger = winston.createLogger({
+//     transports: [
+//         new winston.transports.Console({level:'http'}),
+//         new winston.transports.File({filename:'./errors.log', level: 'warn'})
+//     ]
+// })
 const logger = winston.createLogger({
+    levels: customLevelsOptions.levels,
     transports: [
-        new winston.transports.Console({level:'http'}),
-        new winston.transports.File({filename:'./errors.log', level: 'warn'})
+        new winston.transports.Console({level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize({colors: customLevelsOptions.colors}),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename:'./errors.log', 
+            level: 'warning',
+            format: winston.format.simple()
+        })
     ]
 })
 
 exports.addLogger = (req, res, next) => {
     req.logger = logger
-    req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
+    req.logger.info(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
     next()
 }
